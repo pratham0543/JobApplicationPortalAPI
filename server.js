7a36ff9f7d0d4c37091310a501dc4f02e1298cc7
pratham0543 <pratham0543.be20@chitkara.edu.in>
2022-12-18 21:20:56 +0530

    updated

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http=require("http");
 const app=require("./app")
-const PORT=process.env.port||5001;
+const PORT=process.env.PORT||5001;
 const server=http.createServer(app);
 server.listen(PORT);
